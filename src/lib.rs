023f8ea20f21fd68fabc6e8c2766c39720669d7f
Amr Ali <amralicc@gmail.com>
2022-11-28 03:35:12 +0200

    Remove wee_alloc and use libcore::alloc instead

--- src/lib.rs
+++ src/lib.rs
@@ -49,10 +49,6 @@
 
 extern crate alloc;
 
-// Use wee_alloc as the global allocator.
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 mod wave;
 mod waveform;
 
